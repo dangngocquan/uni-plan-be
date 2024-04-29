4fad649cae7b5b9b0a78c7a27d16000b3d3b849f
dangngocquan <dangngocquan104@gmail.com>
2024-04-29 22:55:47 +0700

    Testing vercel

--- src/modules/grade-conversion/dto/res.page.conversion-table.dto.ts
+++ src/modules/grade-conversion/dto/res.page.conversion-table.dto.ts
@@ -1,16 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ResGradeConversionTableDto } from './res.conversion-table.dto';
-import { Pagination0Dto } from 'src/modules/pagination/pagination0.dto';
+import { PaginationMetaDto } from 'src/modules/pagination/meta.dto';
+import { PaginationLinkDto } from 'src/modules/pagination/links.dto';
 
-export class PaginationConversionTableDto extends Pagination0Dto {
+export class PaginationConversionTableDto {
   @ApiProperty({
     isArray: true,
     type: ResGradeConversionTableDto,
   })
   items: ResGradeConversionTableDto[];
 
+  @ApiProperty({
+    type: PaginationMetaDto,
+  })
+  meta: PaginationMetaDto;
+
+  @ApiProperty({
+    type: PaginationLinkDto,
+  })
+  links: PaginationLinkDto;
+
   constructor(items, meta, links) {
-    super(meta, links);
     this.items = items;
+    this.meta = meta;
+    this.links = links;
   }
 }
